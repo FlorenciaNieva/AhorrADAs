6c11802a561a23a4ff3b4d23178b9d0d4f25d401
FlorenciaNieva <florencianieva930@gmail.com>
2023-09-08 21:54:05 -0300

    fix: optimización de líneas de código en eventos de intercambio de paneles con función mostrarVista

--- script.js
+++ script.js
@@ -8,27 +8,11 @@ const mostrarVista = (vistaAMostrar) => {
     $(`#${vistaAMostrar}`).classList.remove('is-hidden');
 };
 
-$('#boton-balance').addEventListener('click', () => {
-    $('#seccion-balance').classList.remove('is-hidden');
-    $('#seccion-categorias').classList.add('is-hidden');
-    $('#seccion-reportes').classList.add('is-hidden');
-    $('#seccion-nueva-operacion').classList.add('is-hidden');
-});
+$('#boton-balance').addEventListener('click', () => mostrarVista('seccion-balance'));
 
-$('#boton-categorias').addEventListener('click', () => {
-    $('#seccion-categorias').classList.remove('is-hidden');
-    $('#seccion-balance').classList.add('is-hidden');
-    $('#seccion-reportes').classList.add('is-hidden');
-    $('#seccion-nueva-operacion').classList.add('is-hidden');
-});
+$('#boton-categorias').addEventListener('click', () => mostrarVista('seccion-categorias'));
 
-$('#boton-reportes').addEventListener('click', () => {
-    $('#seccion-reportes').classList.remove('is-hidden');
-    $('#sin-reportes').classList.remove('is-hidden');
-    $('#seccion-categorias').classList.add('is-hidden');
-    $('#seccion-balance').classList.add('is-hidden');
-    $('#seccion-nueva-operacion').classList.add('is-hidden');
-});
+$('#boton-reportes').addEventListener('click', () => mostrarVista('seccion-reportes'));
 
 // boton para ocultar o mostrar los filtros
 
